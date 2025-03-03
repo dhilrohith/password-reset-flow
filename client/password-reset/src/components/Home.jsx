ab3ce6f81a9b004b8fc4ebe8152e4d65be30c53f
dhilrohith <rohithbj2018@gmail.com>
2025-03-03 19:00:40 +0530

    styles updated in home.jsx

--- client/password-reset/src/components/Home.jsx
+++ client/password-reset/src/components/Home.jsx
@@ -5,12 +5,12 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold mb-8">Welcome to Our Application!</h1>
+      <h1 className="text-4xl font-bold mb-8 text-center">Welcome to Our Application!</h1>
       <div className="space-x-4">
         <Link 
           to="/login" 
           className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded transition
-          focus:outline-2 focus:outline-offset-2 focus:outline-green-500 active:bg-green-700"
+          focus:outline-2 focus:outline-offset-2 focus:outline-blue-500 active:bg-blue-700"
         >
           Login
         </Link>
